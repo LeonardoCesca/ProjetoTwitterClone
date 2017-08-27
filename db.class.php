32e0cadc8508819590d05b217f7c430151a5d01a
LeonardoCesca <leonardocescaf@gmail.com>
2017-08-27 10:17:07 -0300

    arrumando conexao

--- db.class.php
+++ db.class.php
@@ -18,7 +18,18 @@ class db{
 	public function conecta_mysql(){
 		
 		//criar conexao
-		mysqli_connect()//espera 4 parametros(localizacao do bd, usuario de acesso, senha, banco de dados)//
+		$con = mysqli_connect($this->host, $this->usuario, $this->senha, $this->database)//espera 4 parametros(localizacao do bd, usuario de acesso, senha, banco de dados)//$this faz referencia a propriedade existente dentro da propria classe.
+		
+		//ajustar o charset de comunicacao entre a aplicacao e o BD.
+		mysqli_set_charset($con, 'utf8');
+		
+		//verificar se houve erro de conexão
+		if(mysqli_connect_errno()){
+			echo 'Erro ao tentar se conectar no Banco de Dados: ' .mysqli_connect_error();//ajuda no debugg...
+			
+		}
+		
+		return $con;
 	}
 	
 }
