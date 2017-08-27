f5fae37c7af7fe0b07bb6ad2be0cd7373050dc4c
LeonardoCesca <leonardocescaf@gmail.com>
2017-08-27 18:29:42 -0300

    .

--- db.class.php
+++ db.class.php
@@ -1,36 +1,36 @@
 <?php
-class db{
-	
-	//host
-	private $host = 'localhost';
-	
-	//usuario
-	private $usuario = 'root';
-	
-	//senha
-	private $senha = '';
-	
-	//banco de dados
-	private $database = 'twitter_clone';
-	
-	//funcao que vai executar a conexao entre php e mysql
-	
-	public function conecta_mysql(){
-		
-		//criar conexao
-		$con = mysqli_connect($this->host, $this->usuario, $this->senha, $this->database)//espera 4 parametros(localizacao do bd, usuario de acesso, senha, banco de dados)//$this faz referencia a propriedade existente dentro da propria classe.
-		
-		//ajustar o charset de comunicacao entre a aplicacao e o BD.
-		mysqli_set_charset($con, 'utf8');
-		
-		//verificar se houve erro de conexão
-		if(mysqli_connect_errno()){
-			echo 'Erro ao tentar se conectar no Banco de Dados: ' .mysqli_connect_error();//ajuda no debugg...
-			
-		}
-		
-		return $con;
-	}
-	
-}
+
+    class db {
+
+        //host
+        private $host = 'localhost';
+
+        //usuario
+        private $usuario = 'root';
+
+        //senha
+        private $senha = '';
+
+        //banco de dados
+        private $database = 'twitter_clone';
+
+        public function conecta_mysql(){
+
+            //criar conexao
+            $con = mysqli_connect($this->host, $this->usuario, $this->senha, $this->database);
+
+            //ajustar o charset de comunicação entre a aplicação e o banco de dados
+            mysqli_set_charset($con, 'utf8');
+
+            //verificar se houve erro de conexão
+            if(mysqli_connect_errno()){
+                echo 'Erro ao tentar se conectar com BD MySQL: ' . mysqli_connect_error();
+            }
+
+            return $con;
+
+        }
+
+    }
+
 ?>
