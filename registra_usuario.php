b42c5c9e5ddf951dbb59b1a627b0cd041219df8d
LeonardoCesca <leonardocescaf@gmail.com>
2017-08-29 00:14:45 -0300

    bloqueando inscrever usuarios duplicados

--- registra_usuario.php
+++ registra_usuario.php
@@ -8,7 +8,54 @@
 
 	$objDb = new db();
 	$link = $objDb->conecta_mysql();
+	
+	$usuario_existe = false;
+	$email_existe = false;
 
+	//verificar se usuario já existe
+	$sql = " select * from usuarios where usuario = '$usuario' ";
+		if($resultado_id = mysqli_query($link, $sql)){ //retorno atribuido a variavel $resultado_id;
+		
+			$dados_usuario = mysqli_fetch_array($resultado_id);
+			
+			if(isset ($dados_usuario['usuario'])){
+				$usuario_existe = true;
+			}	
+			
+		}else{
+		echo 'Erro ao tentar localizar o registro do usuário';
+	}
+	
+	//verificar se o email já existe
+	$sql = " select * from usuarios where email = '$email' ";
+		if($resultado_id = mysqli_query($link, $sql)){ //retorno atribuido a variavel $resultado_id;
+		
+			$dados_usuario = mysqli_fetch_array($resultado_id);
+		
+			if(isset ($dados_usuario['email'])){
+				$email_existe = true;
+			}
+		}else{
+		echo 'Erro ao tentar localizar o registro do usuário';
+	}
+	
+	if($usuario_existe || $email_existe){
+		
+		$retorno_get = '';
+		
+		if($usuario_existe){
+			$retorno_get.= "erro_usuario=1&";
+		}
+		
+		if($email_existe){
+			$retorno_get.= "erro_email=1&";
+		}
+		
+		header('Location: inscrevase.php?'.$retorno_get);
+		die(); //interrompe a funcao do script
+
+	}
+	
 	$sql = " insert into usuarios(usuario, email, senha) values ('$usuario', '$email', '$senha') ";
 
 	//executar a query
